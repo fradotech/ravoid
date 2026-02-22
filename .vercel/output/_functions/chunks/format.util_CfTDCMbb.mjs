45891db1c502a206788b7f64690ee9c6291dba48
fradotech <fradotech.id@gmail.com>
2026-02-22 11:27:24 +0700

    fix: remove .vercel

--- .vercel/output/_functions/chunks/format.util_CfTDCMbb.mjs
+++
@@ -1,9 +0,0 @@
-function truncate(text, maxLength) {
-  if (text.length <= maxLength) return text;
-  return text.slice(0, maxLength).trimEnd() + "...";
-}
-function pluralize(count, singular, plural) {
-  return count === 1 ? `${count} ${singular}` : `${count} ${singular + "s"}`;
-}
-
-export { pluralize as p, truncate as t };
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_lh_R4JpA.mjs';
-export { renderers } from '../renderers.mjs';
