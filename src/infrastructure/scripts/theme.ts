4e0adf3a1e449e43824d773f3212a9d1eef78388
fradotech <fradotech.id@gmail.com>
2026-03-29 06:41:11 +0700

    feat: refactor custom scripts

--- src/infrastructure/scripts/theme.ts
+++
@@ -1,6 +0,0 @@
-const theme = localStorage.getItem('theme');
-if (theme === 'light') {
-  document.documentElement.classList.remove('dark');
-} else {
-  document.documentElement.classList.add('dark');
-}
